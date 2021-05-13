c81a7c9f76981fac84db6bbc88184a32cadf4a9d
osmandagli <osman.dagli687@gmail.com>
2021-05-13 11:22:10 +0300

    house defuzz

--- fuzzy.py
+++ fuzzy.py
@@ -49,6 +49,7 @@ ca_low = fuzz.membership.trimf(x_credit_amount, [0, 125, 250])
 ca_medium = fuzz.membership.trimf(x_credit_amount, [125, 250, 375])
 ca_high = fuzz.membership.trimf(x_credit_amount, [250, 375, 500])
 ca_vh = fuzz.membership.trimf(x_credit_amount, [375, 500, 500])
+
 '''
 fig,  (ax0,ax1,ax2,ax3) = plt.subplots(nrows = 4, figsize = (20, 20))
 
@@ -113,8 +114,9 @@ ax7.legend()
 plt.show()
 '''
 
-input_mvh = 60
-input_loc = 3
+input_mvh = 500
+input_loc = 5
+#input_aa = 5
 
 #.append(fuzz.interp_membership(x_, , ))
 
@@ -138,21 +140,58 @@ loc_dict = dict(zip(d, loc_fit))
 #print(loc_dict)
 
 '''
+asset_applicants = [aa_low, aa_medium, aa_high]
+asset_fit = []
+for applicant in asset_applicants:
+    asset_fit.append(fuzz.interp_membership(x_asset_application, applicant, input_aa))
+'''
+
+AND = min
+OR = max
+
+'''
     House eval rules
 '''
-AND = np.min
-OR = np.max
+
+rule_n3 = AND(loc_dict['bad'], mvh_dict['low']) * house_vl
 
 rule_n1 = mvh_dict['low'] * house_low
 rule_n2 = loc_dict['bad'] * house_low
+rule_n4 = AND(loc_dict['bad'], mvh_dict['medium']) * house_low
+rule_n7 = AND(loc_dict['fair'], mvh_dict['low']) * house_low
 
-r1 = [max(a,b) for a,b in zip(rule_n1,rule_n2)] 
+rule_n5 = AND(loc_dict['bad'], mvh_dict['high']) * house_medium
+rule_n8 = AND(loc_dict['fair'], mvh_dict['medium']) * house_medium
+rule_n11 = AND(loc_dict['exc'] , mvh_dict['low']) * house_medium
 
-#r2 = OR(mvh_dict['low'], loc_dict['bad']) * house_low
+rule_n9 = AND(loc_dict['fair'], mvh_dict['high']) * house_high
+rule_n12 = AND(loc_dict['exc'] , mvh_dict['medium']) * house_high
+rule_n6 = AND(loc_dict['bad'], mvh_dict['vh']) * house_high
 
-plt.plot(x_house, house_low)
-plt.plot(x_house, r1)
-#plt.plot(x_house, r2)
+rule_n10 = AND(loc_dict['fair'] , mvh_dict['vh']) * house_vh
+rule_n13 = AND(loc_dict['exc'] , mvh_dict['high']) * house_vh
+rule_n14 = AND(loc_dict['exc'] , mvh_dict['vh']) * house_vh
+
+out_house_vl = rule_n3
+out_house_low = [max(a,b,c,d) for a,b,c,d in zip(rule_n1,rule_n2,rule_n4,rule_n7)] 
+out_house_medium = [max(a,b,c) for a,b,c in zip(rule_n5,rule_n8,rule_n11)] 
+out_house_high = [max(a,b,c) for a,b,c in zip(rule_n6,rule_n9,rule_n12)] 
+out_house_vh = [max(a,b,c) for a,b,c in zip(rule_n10,rule_n13,rule_n14)] 
+
+house_out = np.array([max(a,b,c,d,e) for a,b,c,d,e in zip(out_house_vl, out_house_low, out_house_medium, out_house_high, out_house_vh)])
+
+'''
+    Applicant eval rules
+'''
+
+
+
+plt.plot(x_house, house_out)
+plt.plot(x_house, out_house_high)
+
+defuzz_mom = fuzz.defuzz(x_house, house_out, 'mom')
+#membership_res = fuzz.interp_membership(x_house, house_out, defuzz_mom) içinde bulunma oranı
+print(defuzz_mom)
 
 plt.show()
 
